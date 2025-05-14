4b9c87fc27cd570049e6e4e6ec7b65234bee82ee
Ayushi <ayushiiijani@gmail.com>
2025-05-14 14:25:14 +0530

    created user model and user repository

---
+++ StudentRegistrationSystem/src/main/java/com/example/StudentRegistrationSystem/model/User.java
@@ -0,0 +1,27 @@
+package com.example.StudentRegistrationSystem.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import lombok.Data;
+import jakarta.persistence.Id;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    private long userId;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role userRole;
+
+    private boolean status;
+
+    public enum Role {
+        Student, Faculty, Admin
+    }
+}
+
---
+++ StudentRegistrationSystem/src/main/java/com/example/StudentRegistrationSystem/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.StudentRegistrationSystem.repository;
+
+import com.example.StudentRegistrationSystem.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserId(String userId);
+}
+
